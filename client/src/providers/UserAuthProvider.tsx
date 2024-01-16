71df120116a3df5dd2d6f9d10df2bba0fa9632b2
Patel Divyesh <pateldivyesh1323@gmail.com>
2024-01-16 17:51:28 +0530

    Removed unused code block

--- client/src/providers/UserAuthProvider.tsx
+++ client/src/providers/UserAuthProvider.tsx
@@ -1,14 +1,11 @@
 /* eslint-disable react-refresh/only-export-components */
 import { FC, ReactNode, createContext, useContext, useEffect, useState } from "react";
-import { UserInterface } from "../models/user";
-import { useAuth0 } from "@auth0/auth0-react";
-import enviroment from "../enviroment";
-import axios from "axios";
+import { User, useAuth0 } from "@auth0/auth0-react";
 import { getErrorMsg } from "../utils/error";
 import { useNavigate } from "react-router-dom";
 
 interface UserAuthContextInterface {
-    user?: UserInterface;
+    user?: User;
     error?: string;
     isLoading: boolean;
     isAuthenticated: boolean;
@@ -25,16 +22,14 @@ const UserAuthContext = createContext<UserAuthContextInterface | null>(null);
 
 export const UserAuthProvider: FC<PropsType> = ({ children }) => {
 
-    const { loginWithPopup, logout, isAuthenticated, isLoading, error: auth0Error, getAccessTokenSilently, user: auth0User } = useAuth0();
+    const { loginWithPopup, logout, isAuthenticated, isLoading, error: auth0Error, getAccessTokenSilently, user } = useAuth0();
     const navigate = useNavigate();
 
-    const [user, setUser] = useState<UserInterface | undefined>();
     const [error, setError] = useState<string | undefined>();
 
     useEffect(() => {
         if (auth0Error?.message) {
             setError(auth0Error.message);
-            setUser(undefined);
         }
     }, [auth0Error]);
 
@@ -42,19 +37,7 @@ export const UserAuthProvider: FC<PropsType> = ({ children }) => {
         try {
             await loginWithPopup();
             navigate("/passwords");
-            if (auth0User) {
-                const token = await getAccessTokenSilently();
-                const res = await axios.post(`${enviroment.server_url}/user/login`,
-                    auth0User,
-                    {
-                        headers: { Authorization: `Bearer ${token}` }
-                    }
-                )
-                setUser(res.data);
-                setError(undefined);
-            }
         } catch (error: unknown) {
-            setUser(undefined);
             setError(getErrorMsg(error));
         }
     }
